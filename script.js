6981c5b5f195953f8236edc2c792ae207f5ebde6
Anna manoelly <anna.mannuuh73@gmail.com>
2024-02-08 21:41:15 -0300

    commit final

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('mobile-menu').addEventListener('click', function () {
+    var navbar = document.querySelector('.navbar');
+    navbar.style.display = (navbar.style.display === 'flex') ? 'none' : 'flex';
+});
+
+
+
